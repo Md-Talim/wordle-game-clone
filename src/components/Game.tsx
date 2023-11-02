e57541427108e5e846a0b3438685e984cedd093f
Mohammad Talim <talimmohammad116@gmail.com>
2023-11-02 14:08:37 +0530

    Refactor: extract guessInput as a new component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,51 +1,18 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { useState } from 'react';
 import GuessResults from './GuessResults';
+import GuessInput from './GuessInput';
 
 const Game = ({ answer }: { answer: string }) => {
-  const [value, setValue] = useState('');
   const [guesses, setGuesses] = useState<string[]>([]);
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-
-    // If the length of the guessed input is not equal to 5
-    // then don't do anything else
-    if (value.length !== 5) {
-      window.alert('Please enter exactly 5 letters.');
-      return;
-    }
-
-    // Add the entered value to the guesses array
-    if (guesses.length <= 0) {
-      setGuesses([value]);
-    } else {
-      setGuesses([...guesses, value]);
-    }
-
-    setValue('');
-  };
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const nextGuess = event.target.value.toUpperCase();
-
-    setValue(nextGuess);
+  const handleSubmitGuess = (newGuess: string) => {
+    setGuesses([...guesses, newGuess]);
   };
 
   return (
     <>
       <GuessResults guesses={guesses} answer={answer} />
-      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
-        <label htmlFor="guess-input">Enter guess:</label>
-        <input
-          id="guess-input"
-          maxLength={5}
-          minLength={5}
-          onChange={handleChange}
-          required
-          type="text"
-          value={value}
-        />
-      </form>
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
 };
---
+++ src/components/GuessInput.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+interface Props {
+  handleSubmitGuess: (guess: string) => void;
+}
+
+const GuessInput = ({ handleSubmitGuess }: Props) => {
+  const [newGuess, setNewGuess] = useState('');
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    // If the length of the guessed input is not equal to 5
+    // then don't do anything else
+    if (newGuess.length !== 5) {
+      window.alert('Please enter exactly 5 letters.');
+      return;
+    }
+
+    // Add the guess to the guesses array
+    handleSubmitGuess(newGuess);
+
+    setNewGuess('');
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const nextGuess = event.target.value.toUpperCase();
+
+    setNewGuess(nextGuess);
+  };
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        maxLength={5}
+        minLength={5}
+        onChange={handleChange}
+        required
+        type="text"
+        value={newGuess}
+      />
+    </form>
+  );
+};
+
+export default GuessInput;
